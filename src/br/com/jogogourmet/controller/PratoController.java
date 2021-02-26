e3ed7da38fa6bc663a5a95ffadddf4327d1e9b99
Mariana Ramalho <mariana.rcastro26@gmail.com>
2021-02-25 23:53:54 -0300

    Projeto Jogo Gourmet

---
+++ src/br/com/jogogourmet/controller/PratoController.java
@@ -0,0 +1,140 @@
+package br.com.jogogourmet.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+import br.com.jogogourmet.entity.Prato;
+
+public class PratoController
+{
+
+    public List<Prato> listaDePratos;
+    private List<Prato> listaNovosPratos;
+    public static final Prato lasanha = new Prato(1, "Lasanha", "massa");
+    private static final Prato boloDeChocolate = new Prato(2, "Bolo de Chocolate", "doce");
+
+    public PratoController()
+    {
+        listaDePratos = new ArrayList<>();
+        listaNovosPratos = new ArrayList<>();
+    }
+
+    public void adicionandoPratosIniciais()
+    {
+        listaDePratos.add(lasanha);
+        listaDePratos.add(boloDeChocolate);
+    }
+
+    public void descobrirPrato(int opcaoSelecionada)
+    {
+        if(opcaoSelecionada == 0)
+        {
+            int dialogResult3 = JOptionPane.showConfirmDialog(null,"O prato que você pensou é " + lasanha.getNome() + "?", "Confirme", 0, 3);
+            if(dialogResult3 == 0)
+            {
+                JOptionPane.showConfirmDialog(null, "Acertei de novo!!", "Jogo Gourmet", -1, 1);
+            } else
+            {
+                String nomePrato = JOptionPane.showInputDialog(null, "Qual prato vvocê pensou?", "Desisto", 3);
+                if(nomePrato != null)
+                {
+                    JOptionPane.showInputDialog(null, nomePrato + "é ________ mas "+ lasanha.getNome() + " não.", "Desisto", 3);
+                }
+            }
+        } else
+        {
+            int dialogResult2 = JOptionPane.showConfirmDialog(null, "O prato que você pensou é " + boloDeChocolate.getNome() + "?", "Confirme", 0, 3);
+            if(dialogResult2 == 0)
+            {
+                JOptionPane.showConfirmDialog(null, "Acertei de novo!!", "Jogo Gourmet", -1, 1);
+            } else
+            {
+                String nomePrato = JOptionPane.showInputDialog(null, "Qual prato você pensou?", "Desisto", 3);
+                if(nomePrato != null)
+                {
+                    String tipoPrato = JOptionPane.showInputDialog(null, nomePrato + " é ________ mas "+ boloDeChocolate.getNome() + "?", "Desisto", 3);
+                    if(tipoPrato != null)
+                    {
+                        adicionarNovoPrato(nomePrato, tipoPrato);
+                    }
+                }
+            }
+        }
+    }
+
+    public void descobrirPratoDepoisDoPrimeiro(int opcaoSelecionada)
+    {
+        if(opcaoSelecionada == 0)
+        {
+        	int dialogResult3 = JOptionPane.showConfirmDialog(null,"O prato que você pensou é " + lasanha.getNome() + "?", "Confirme", 0, 3);
+            if(dialogResult3 == 0)
+            {
+                JOptionPane.showConfirmDialog(null, "Acertei de novo!!", "Jogo Gourmet", -1, 1);
+            } else
+            {
+                int dialogResult2 = JOptionPane.showConfirmDialog(null, "O prato que você pensou é " + boloDeChocolate.getNome(), "Confirme", 0, 3);
+                if(dialogResult2 == 0)
+                {
+                    JOptionPane.showConfirmDialog(null, "Acertei de novo!!", "Jogo Gourmet", -1, 1);
+                } else
+                {
+                    String nomePrato = JOptionPane.showInputDialog(null, "Qual prato você pensou?", "Desisto", 3);
+                    if(nomePrato != null)
+                    {
+                        String tipoPrato = JOptionPane.showInputDialog(null, nomePrato + " é ________ mas " + boloDeChocolate.getNome() + " não.", "Desisto", 3);
+                        if(tipoPrato != null)
+                        {
+                            adicionarNovoPrato(nomePrato, tipoPrato);
+                        }
+                    }
+                }
+            }
+        } else
+        {
+            int sizeListaNovosPratos = listaNovosPratos.size();
+            int countPratos = 0;
+            while(sizeListaNovosPratos > 0) 
+            {
+                int dialogResult2 = JOptionPane.showConfirmDialog(null, "O prato que você pensou é " + this.listaNovosPratos.get(countPratos).getTipo() + "?", "Confirme", 0, 3);
+                countPratos++;
+                if(dialogResult2 == 0)
+                {
+                    sizeListaNovosPratos = 0;
+                    countPratos = 0;
+                    JOptionPane.showConfirmDialog(null, "Acertei de novo!!", "Jogo Gourmet", -1, 1);
+                }
+                if(countPratos == listaNovosPratos.size())
+                {
+                    int dialogResult4 = JOptionPane.showConfirmDialog(null, "O prato que você pensou é " + boloDeChocolate.getNome(), "Confirme", 0, 3);
+                    if(dialogResult4 == 0)
+                    {
+                        sizeListaNovosPratos = 0;
+                        JOptionPane.showConfirmDialog(null, "Acertei de novo!!", "Jogo Gourmet", -1, 1);
+                    } else
+                    {
+                        String nomePrato = JOptionPane.showInputDialog(null, "Qual prato você pensou?", "Desisto", 3);
+                        if(nomePrato != null)
+                        {
+                        	String tipoPrato = JOptionPane.showInputDialog(null, nomePrato + " é ________ mas " + boloDeChocolate.getNome() + " não.", "Desisto", 3);
+                            if(tipoPrato != null)
+                            {
+                                sizeListaNovosPratos = 0;
+                                adicionarNovoPrato(nomePrato, tipoPrato);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void adicionarNovoPrato(String nome, String tipo)
+    {
+        int id = listaDePratos.size() + 1;
+        Prato novoPrato = new Prato(id, nome, tipo);
+        listaNovosPratos.add(novoPrato);
+        listaDePratos.add(novoPrato);
+    }
+
+}
---
+++ src/br/com/jogogourmet/view/TelaPrincipalFrame.java
@@ -0,0 +1,126 @@
+package br.com.jogogourmet.view;
+
+import javax.swing.JOptionPane;
+
+import br.com.jogogourmet.controller.PratoController;
+
+/**
+ *
+ * @author Mariana
+ */
+public class TelaPrincipalFrame extends javax.swing.JFrame {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+    public TelaPrincipalFrame() {
+        initComponents();
+        pratoController.adicionandoPratosIniciais();
+    }
+    
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAutoRequestFocus(false);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel1.setText("Pense em um prato que gosta");
+
+        jButton1.setText("OK");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(23, 23, 23)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(86, 86, 86)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(61, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(32, 32, 32)
+                .addComponent(jButton1)
+                .addGap(28, 28, 28))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>                        
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+    	 int dialogResult = JOptionPane.showConfirmDialog(null, "O prato que você pensou é " + PratoController.lasanha.getTipo() + "?", "Confirme", 0, 3);
+         if(pratoController.listaDePratos.size() < 3)
+         {
+             pratoController.descobrirPrato(dialogResult);
+         } else
+         {
+             pratoController.descobrirPratoDepoisDoPrimeiro(dialogResult);
+         }
+    }                                        
+
+    public static void main(String args[]) {
+       
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaPrincipalFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaPrincipalFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaPrincipalFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaPrincipalFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaPrincipalFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration                   
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private final PratoController pratoController = new PratoController();
+    // End of variables declaration                   
+}
